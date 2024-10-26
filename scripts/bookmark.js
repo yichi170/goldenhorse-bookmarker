10b178ab2b614bf8d367d3f3d4118daffaedab80
yichi170 <yichi170@gmail.com>
2024-10-26 12:05:12 -0500

    setup project struture

---
+++ scripts/bookmark.js
@@ -0,0 +1,8 @@
+function addBookmarkButtons() {
+    // Select all screening rows
+    const rows = document.querySelectorAll(".table.special.show-list tbody tr.text-center");
+
+    rows.forEach((row) => {});
+}
+
+addBookmarkButtons();
---
+++ scripts/highlight.js
@@ -0,0 +1,4 @@
+function highlightScreenings() {}
+
+
+highlightScreenings();

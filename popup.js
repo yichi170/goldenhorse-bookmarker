725bc0a8f14ef04edec2bef1fdc775824bfdcf38
yichi170 <yichi170@gmail.com>
2024-10-27 23:01:33 -0500

    beautify the popup window

--- popup.js
+++ popup.js
@@ -14,8 +14,18 @@ document.addEventListener("DOMContentLoaded", () => {
                     const listItem = document.createElement("li");
 
                     listItem.innerHTML = `
-                        <a href="${screening.link}" target="_blank">${screening.title} at ${screening.date} ${screening.time}</a>
-                        <button class="delete-button" data-index="${index}">Delete</button>
+                        <div class="bookmark-card">
+                            <div class="bookmark-content">
+                                <a href="${screening.link}" target="_blank">${screening.title}</a>
+                                <p>${screening.date} ${screening.time}</p>
+                            </div>
+                            <div class="delete-button" data-index="${index}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+                                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                                </svg>
+                            </div>
+
+                        </div>
                     `;
 
                     bookmarkList.appendChild(listItem);
